f14da0f7d3de34eff913a43796a3408a5763bf64
GRF75AR <grf75ar@yahoo.com.ar>
2022-09-26 19:51:14 -0300

    load scene / exit

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuHandler : MonoBehaviour
+{
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        //MainManager.Instance.SaveColor();
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+}
